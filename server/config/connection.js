dcde386b3072e2263767bcdb42c464bd76af5990
Michael Correa <sales@micomar.com>
2022-11-04 15:13:39 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 module.exports = mongoose.connection;
